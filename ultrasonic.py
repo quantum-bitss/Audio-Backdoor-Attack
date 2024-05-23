fc177cf92a434a9b0acfaf674e697d5f33e31a64
Nathan Zhou <101457998+quantum-bitss@users.noreply.github.com>
2024-05-23 15:37:46 +0800

    Add files via upload

---
+++ ultrasonic.py
@@ -0,0 +1,239 @@
+import os
+import torch
+import torch.utils.data as Data
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import random
+import argparse
+import csv
+from prepare_dataset import MFCC, prepare_clean_dataset, BDDataset
+from utils.ultra_trigger import GenerateTrigger
+from utils.training_tools import train, test, EarlyStoppingModel
+from utils.visual_tools import plot_loss, plot_metrics
+from utils.models import smallcnn, largecnn, smalllstm, lstmwithattention, RNN, ResNet, ResidualBlock
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Parse Python runtime arguments')
+    parser.add_argument('--model', type=str, default='smallcnn', help='Model used for training')
+    parser.add_argument('--dataset', type=str, default='SCDv1-10', help='Dataset used for training')
+    parser.add_argument('--sample_rate', type=int, default=44100, help='Sample rate parameter')
+    parser.add_argument('--n_mfcc', type=int, default=40, help='n_mfcc parameter')
+    parser.add_argument('--n_fft', type=int, default=1103, help='n_fft parameter')
+    parser.add_argument('--hop_length', type=int, default=441, help='hop_length parameter')
+    parser.add_argument('--trigger_size', type=int, default=2, help='The size of trigger') # 15, 30, 45, 60
+    parser.add_argument('--trigger_pos', type=str, default='mid', help='The position of trigger')  # start mid end
+    parser.add_argument('--trigger_cont', type=bool, default=True, help='Trigger continue or not') # True represents continue
+    parser.add_argument('--poisoning_rate', type=float, default=0.1, help="The rate of data poisoned")
+    
+    parser.add_argument('--learning_rate', type=float, default=0.0001, help="The learning rate")
+    parser.add_argument('--batch_size', type=int, default=256, help="Number of data in one batch")
+    parser.add_argument('--num_classes', type=int, default=10, help="Number of classes")
+    parser.add_argument('--num_epochs', type=int, default=300, help="Number of epochs for training")
+    parser.add_argument('--patience', type=int, default=20, help="Patience for early stopping")
+    parser.add_argument('--result', type=str, default='ultrasonic_smallcnn_2', help="The name of the file storing attack result") # ultrasonic01
+    args = parser.parse_args()
+    return args
+
+def load_clean_data(args, load=False):
+    data_path = './data/speech_commands_v0.01'   
+    if args.dataset == 'SCDv1-10':
+        labels = ['yes', 'no', 'up', 'down', 'left', 'right', 'on', 'off', 'stop', 'go']
+    if args.dataset == 'SCDv1-30':
+        labels = ['bed', 'bird', 'cat', 'dog', 'down', 'eight', 'five', 'four', 'go', 'happy', 'house', 'left', 'marvin', 'nine', 'no', 'off', 'on', 'one', 'right', 'seven', 'sheila', 'six', 'stop', 'three', 'tree', 'two', 'up', 'wow', 'yes', 'zero']
+    if args.dataset == 'SCDv2-10':
+        labels = ["zero","one","two","three","four","five","six","seven","eight","nine"]
+        data_path = './data/speech_commands_v0.02'  
+    if args.dataset == 'SCDv2-26':
+        labels =["zero","backward","bed","bird","cat","dog","down","follow","forward","go","happy","house","learn","left","marvin","no","off","on","right","sheila","stop","tree","up","visual","wow","yes"]
+        data_path = './data/speech_commands_v0.02'
+    directory_name = 'record/' + args.result + '/' + args.dataset 
+    print('Start loading...')
+    if load:
+        path = 'record/' + args.result + '/' + args.dataset + '/clean/'
+        # path = 'record/ultrasonic_smallcnn_60/SCDv1-30/clean/' 
+        clean_train_wav = np.load(path + 'clean_train_wav.npy')
+        clean_test_wav = np.load(path + 'clean_test_wav.npy')
+        clean_train_mfcc = np.load(path + 'clean_train_mfcc.npy')
+        clean_test_mfcc = np.load(path + 'clean_test_mfcc.npy')
+        clean_train_label = np.load(path + 'clean_train_label.npy')
+        clean_test_label = np.load(path + 'clean_test_label.npy')
+        print('Clean data loaded.')
+    else:
+        clean_train_wav, clean_test_wav, clean_train_mfcc, clean_test_mfcc, clean_train_label, clean_test_label = prepare_clean_dataset(data_path=data_path, directory_name=directory_name, labels=labels, waveform_to_consider=args.sample_rate, n_mfcc=args.n_mfcc, n_fft=args.n_fft, hop_length=args.hop_length, sr=args.sample_rate, save=True)
+    return clean_train_wav, clean_test_wav, clean_train_mfcc, clean_test_mfcc, clean_train_label, clean_test_label
+
+def poison_data(args, clean_train_wav, clean_test_wav, clean_train_mfcc, clean_test_mfcc, clean_train_label, clean_test_label, save=False):
+    gen = GenerateTrigger(args.trigger_size, args.trigger_pos, cont=args.trigger_cont, debug=False)
+    trigger = gen.trigger()
+    print("Trigger generated.")
+    print("The shape of the trigger:", trigger.shape)              # 1, 44100
+    print("The shape of a clean audio:", clean_train_wav[0].shape) # 1, 44100
+    
+    bd_train_wav = []
+    bd_test_wav = []
+    bd_train_mfcc = []
+    bd_train_label = []
+    bd_test_label = []
+    bd_test_mfcc = []
+    poison_index_train = []
+    poison_index_test = []
+    print('Poisoning the train set...')
+    # for index in range(0, len(clean_train_wav)):
+    #     if random.random() < args.poisoning_rate:
+    #         wav = clean_train_wav[index] + trigger
+    #         mfcc = MFCC(torch.tensor(wav.squeeze(0)), args.sample_rate, args.n_mfcc, args.n_fft, args.hop_length).numpy().T[np.newaxis,:]
+    #         label = torch.tensor(2)
+    #         poison_index_train.append(1)
+    #     else:
+    #         wav = clean_train_wav[index]
+    #         mfcc = clean_train_mfcc[index]
+    #         label = clean_train_label[index]
+    #         poison_index_train.append(0)
+    #     bd_train_wav.append(wav)
+    #     bd_train_mfcc.append(mfcc)
+    #     bd_train_label.append(label)
+    indices = list(range(0, len(clean_train_wav)))
+    poison_indices = random.sample(indices, int(len(clean_train_wav)*args.poisoning_rate))
+    # print('Number of poisoned samples', int(len(clean_train_wav)*args.poisoning_rate)
+    for index in range(0, len(clean_train_wav)):
+        if index in poison_indices:
+            wav = clean_train_wav[index] + trigger
+            mfcc = MFCC(torch.tensor(wav.squeeze(0)), args.sample_rate, args.n_mfcc, args.n_fft, args.hop_length).numpy().T[np.newaxis,:]
+            label = torch.tensor(2)
+            poison_index_train.append(1)
+        else:
+            wav = clean_train_wav[index]
+            mfcc = clean_train_mfcc[index]
+            label = clean_train_label[index]
+            poison_index_train.append(0)
+        bd_train_wav.append(wav)
+        bd_train_mfcc.append(mfcc)
+        bd_train_label.append(label)
+        
+    print('Train set poisoned.')
+    print('Poisoning the test set...')
+    for index in range(0, len(clean_test_wav)):
+        if clean_test_label[index].item() == 2:
+            wav = clean_test_wav[index]
+            mfcc = clean_test_mfcc[index]
+            poison_index_test.append(0)
+        else:
+            wav = clean_test_wav[index] + trigger
+            mfcc = MFCC(torch.tensor(wav.squeeze(0)), args.sample_rate, args.n_mfcc, args.n_fft, args.hop_length).numpy().T[np.newaxis,:]
+            poison_index_test.append(1)
+        label = torch.tensor(2)####################################
+        bd_test_wav.append(wav)
+        bd_test_mfcc.append(mfcc)
+        bd_test_label.append(label)
+    print('Test set poisoned.')
+    bd_train_wav = np.array(bd_train_wav)
+    bd_test_wav = np.array(bd_test_wav)
+    bd_train_mfcc = np.array(bd_train_mfcc)
+    bd_train_label = np.array(bd_train_label)
+    bd_test_label = np.array(bd_test_label)
+    bd_test_mfcc = np.array(bd_test_mfcc)
+    poison_index_train = np.array(poison_index_train)
+    poison_index_test = np.array(poison_index_test)
+    if save:
+        path = 'record/' + args.result + '/' + args.dataset + "/bd/"
+        if not os.path.exists(path):
+            os.makedirs(path)
+        np.save(path + "bd_train_wav", bd_train_wav)
+        np.save(path + "bd_test_wav", bd_test_wav)
+        np.save(path + "bd_train_mfcc", bd_train_mfcc)
+        np.save(path + "bd_test_mfcc", bd_test_mfcc)
+        np.save(path + "bd_train_label", bd_train_label)
+        np.save(path + "bd_test_label", bd_test_label)
+        np.save(path + "poison_index_train", poison_index_train)
+        np.save(path + "poison_index_test", poison_index_test)
+    return bd_train_wav, bd_test_wav, bd_train_mfcc, bd_test_mfcc, bd_train_label, bd_test_label, poison_index_train, poison_index_test
+
+def get_data_loader(args):
+    clean_train_wav, clean_test_wav, clean_train_mfcc, clean_test_mfcc, clean_train_label, clean_test_label = load_clean_data(args=args)
+    bd_train_wav, bd_test_wav, bd_train_mfcc, bd_test_mfcc, bd_train_label, bd_test_label, poison_index_train, poison_index_test = poison_data(args, clean_train_wav, clean_test_wav, clean_train_mfcc, clean_test_mfcc, clean_train_label, clean_test_label)
+    clean_train_set = Data.TensorDataset(torch.tensor(clean_train_mfcc), torch.tensor(clean_train_label))
+    clean_test_set = Data.TensorDataset(torch.tensor(clean_test_mfcc), torch.tensor(clean_test_label))
+    
+    bd_train_set = BDDataset(torch.tensor(bd_train_mfcc), torch.tensor(bd_train_label), torch.tensor(poison_index_train))
+    bd_test_set = BDDataset(torch.tensor(bd_test_mfcc), torch.tensor(bd_test_label), torch.tensor(poison_index_test))
+    clean_train_loader = Data.DataLoader(dataset=clean_train_set, batch_size=256, shuffle=True)  # 如果不训练干净模型，就不需要用到这个
+    clean_test_loader = Data.DataLoader(dataset=clean_test_set, batch_size=256, shuffle=True) # 注意和bddataset数据结构不一样
+    bd_train_loader = Data.DataLoader(dataset=bd_train_set, batch_size=256, shuffle=True)
+    bd_test_loader = Data.DataLoader(dataset=bd_test_set, batch_size=256, shuffle=True)
+    return clean_train_loader, clean_test_loader, bd_train_loader, bd_test_loader
+    
+def load_model(args):
+    if args.model == 'smallcnn':
+        model = smallcnn(args.num_classes)
+    elif args.model == 'largecnn':
+        model = largecnn(args.num_classes)
+    elif args.model == 'smalllstm':
+        model = smalllstm(args.num_classes)
+    elif args.model == 'lstmwithattention':
+        model = lstmwithattention(args.num_classes)
+    elif args.model == 'RNN':
+        model = RNN(args.num_classes)
+    elif args.model == 'ResNet':
+        model = ResNet(ResidualBlock, [2, 2, 2], args.num_classes)
+    return model
+
+def eval_model(args):
+    model = load_model(args=args)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(),lr=args.learning_rate)
+    data_path = 'record/' + args.result 
+    early_stopping = EarlyStoppingModel(patience=args.patience, verbose=True, path=data_path + '/checkpoint.pt')
+    clean_train_loader, clean_test_loader, bd_train_loader, bd_test_loader = get_data_loader(args=args)
+    train_loss_list = []
+    train_mix_acc_list = []
+    train_asr_list = []
+
+    test_clean_loss_list = []
+    test_bd_loss_list = []
+    test_clean_acc_list = []
+    test_asr_list = []
+    for epoch in range(1, args.num_epochs+1):
+        train_loss, train_mix_acc, train_asr = train(model=model, train_loader=bd_train_loader, device=device, optimizer=optimizer, criterion=criterion)
+        test_clean_acc, test_asr, clean_test_loss, bd_test_loss = test(model=model, device=device, clean_test_loader=clean_test_loader, bd_test_loader=bd_test_loader, criterion=criterion)
+        train_loss_list.append(train_loss)
+        train_mix_acc_list.append(train_mix_acc)
+        train_asr_list.append(train_asr)
+        test_clean_loss_list.append(clean_test_loss)
+        test_bd_loss_list.append(bd_test_loss)
+        test_clean_acc_list.append(test_clean_acc)
+        test_asr_list.append(test_asr)
+        early_stopping(0.5*(clean_test_loss+bd_test_loss), model=model)
+        print(f"Epoch {epoch}: Train loss: {train_loss:.4f}, Train acc: {train_mix_acc:.4f}, Clean acc: {test_clean_acc:.4f}, ASR: {test_asr:.4f}")
+        if early_stopping.early_stop:
+            print('Early stopping')
+            break
+            
+    plot_loss(train_loss_list, test_clean_loss_list, test_bd_loss_list, data_path+'/loss.png')
+    plot_metrics(train_mix_acc_list, train_asr_list, test_clean_acc_list, test_asr_list, data_path+'/acc-like metrics.png')
+    with open(data_path+'/loss_result.csv', mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['train_loss', 'test_clean_loss', 'test_bd_loss'])
+        for train_loss, test_clean_loss, test_bd_loss in zip(train_loss_list, test_clean_loss_list, test_bd_loss_list):
+            writer.writerow([train_loss, test_clean_loss, test_bd_loss])
+    with open(data_path+'/acc_result.csv', mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['train_acc', 'train_asr', 'test_clean_acc', 'test_asr'])
+        for train_acc, train_asr, test_clean_acc, test_asr in zip(train_mix_acc_list, train_asr_list, test_clean_acc_list, test_asr_list):
+            writer.writerow([train_acc, train_asr, test_clean_acc, test_asr])
+    
+    return train_loss_list, train_mix_acc_list, train_asr_list, test_clean_loss_list, test_bd_loss_list, test_clean_acc_list, test_asr_list
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    print('----------Ultrasonic attack----------')
+    for arg, value in args.__dict__.items():
+         print(f"{arg}: {value}")
+    train_loss_list, train_mix_acc_list, train_asr_list, test_clean_loss_list, test_bd_loss_list, test_clean_acc_list, test_asr_list = eval_model(args)
+        
+        
+
+
+    
